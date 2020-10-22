3f2b54763320001cc5973ccfd3923f22650a9243
ssabur476 <ssabur476@users.noreply.github.com>
2020-10-22 12:32:08 +0300

    Update BasicCalculator.py
    added another function - nearest whole number

--- Python/BasicCalculator.py
+++ Python/BasicCalculator.py
@@ -17,18 +17,24 @@ def div(num1, num2):
 def power(num1,num2):
 	return num1**num2
 
-print("Please Select Operations\n "\
+def wholenum(num1, base=10):
+    return base * round(num1/base)
+
+print("Functions of the Calculator\n "\
 	   "1. Addition\n " 
 	   "2. Subtraction \n"
 	   "3. Multiplication \n"
 	   "4. Divide \n" 
-	   "5. Power of Another Number \n")
+	   "5. Power of Another Number \n"
+	   "6. Display Number to Nearest Whole Number \n")
 
 #Take Input From Keyword
-select = input("Please choose what you want to do")
+select = input("Please Choose Between 1 to 6 from Above: ")
 
-number_1 = int(input("Type Your First Number :"))
-number_2 = int(input("Type YOur Second Number :"))
+number_1 = float(input("Type Your First Number: "))
+number_2 = float(input("Type Your Second Number: "))
+
+
 
 
 if select == '1':
@@ -48,10 +54,16 @@ elif select == '4':
 	print(div(number_1, number_2))
 
 elif select == '5':
-	print(number_1, 'to the power of', number_2, '=')
+	print(number_1, 'to the Power of', number_2, '=')
 	print(power(number_1, number_2))
 
+elif select =='6':
+	print(number_1, 'to Nearest Whole Number', '=')
+	print(wholenum(number_1))
+	print(number_2, 'to Nearest Whole Number', '=')
+	print(wholenum(number_2))
+
 else:
-	print('Invalid Input ! Try aagian ')
+	print('Invalid Input! Please Try Again. ')
 	
-print('Thanks for Using Calculator')
+print('Thank you, Have a Great Day!')
