f00d1edcaa6fcb1fedafeeea57c47e1abe621357
ssabur476 <68954445+ssabur476@users.noreply.github.com>
2020-10-23 22:27:30 +0800

    Update even-odd.py

--- Python/even-odd.py
+++ Python/even-odd.py
@@ -1,5 +1,10 @@
-num = int(input("Enter a number: "))
-if (num % 2) == 0:
-   print("{0} is Even".format(num))
-else:
-   print("{0} is Odd".format(num))
+def nums():
+   num = int(input("Enter a number: "))
+   if (num % 2) == 0:
+      print("The number is Even")
+      print("{0} is Even".format(num))
+   else:
+       print("The number is Odd")
+      print("{0} is Odd".format(num))
+      
+ num()
